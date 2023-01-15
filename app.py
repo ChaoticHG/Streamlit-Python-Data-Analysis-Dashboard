aa9acc1fb31ccb312309e76b633eddbca2559369
ChaoticHG <98245769+ChaoticHG@users.noreply.github.com>
2023-01-15 17:02:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import exploratory,regression # import your app modules here
+from apps import exploratory,regression,classification,arm,clustering # import your app modules here
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app("Exploratory Data Analysis", exploratory.app)
 app.add_app("Regression Models", regression.app)
+app.add_app("Classification Models", classification.app)
+app.add_app("Association Rule Mining", arm.app)
+app.add_app("Clustering Technique", clustering.app)
 # The main app
 app.run()
